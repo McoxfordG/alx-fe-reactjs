d123ae577295e500f8522203b9e4cf1f4573fce2
McoxfordG <mcoxford24@gmail.com>
2024-08-18 08:36:02 +0300

    done

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,20 @@
-import React from "react";
-function UserProfile(props) {
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    );
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  if (!userData) {
+    return <p>No user data available.</p>;
+  }
+
+  return (
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
 }
 
 export default UserProfile;
