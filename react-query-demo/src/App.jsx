fc1c249ab08ebe13aea0462ad77bf40b49d685b5
McoxfordG <mcoxford24@gmail.com>
2024-08-31 01:45:05 +0300

    query demo complete

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PostsComponent from './component/Postscomponent';
+import HomeComponent from './component/HomeComponent';
+// Create a QueryClient instance
+const queryClient = new QueryClient();
+
+function App() {
+    return (
+        // Wrap your app in QueryClientProvider and pass the queryClient
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomeComponent />} />
+                    <Route path="/posts" element={<PostsComponent />} />
+                </Routes>
+            </Router>
+        </QueryClientProvider>
+    );
+}
+
+export default App;
