1103ba7bb5896b8800f8d4f32e3fd87375d34997
McoxfordG <mcoxford24@gmail.com>
2024-08-31 03:35:11 +0300

    Done

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+
+// Fetch function for PostsComponent
+const fetchData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return res.json();
+};
+
+// PostsComponent that uses React Query
+const PostsComponent = () => {
+    const { data, error, isLoading, refetch } = useQuery('fetchData', fetchData);
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error loading data</div>;
+
+    return (
+        <div>
+            <h1>Posts</h1>
+            <button onClick={() => refetch()}>Refetch Posts</button>
+            <Link to="/">Go Home</Link>
+            {data.map(item => (
+                <div key={item.id}>{item.title}</div>
+            ))}
+        </div>
+    );
+};
+
+export default PostsComponent;
